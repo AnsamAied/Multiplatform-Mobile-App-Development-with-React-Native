e0aa7646d5e6a807476a4e8b1c839f606bf8acf1
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-09-25 19:53:31 +0300

    activity  indicator

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import { FlatList } from "react-native";
 import { Tile } from "react-native-elements";
 import { DISHES } from "../shared/dishes";
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-
-const mapStateToProps = state => {
-    return {
-      dishes: state.dishes
-    }
-  }
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+  };
+};
 class Menu extends Component {
   constructor(props) {
     super(props);
@@ -24,23 +24,32 @@ class Menu extends Component {
     const renderMenuItem = ({ item, index }) => {
       return (
         <Tile
-        key={index}
-        title={item.name}
-        caption={item.description}
-        featured
-        onPress={() => navigate('Dishdetail', { dishId: item.id })}
-        imageSrc={{ uri: baseUrl + item.image}}
+          key={index}
+          title={item.name}
+          caption={item.description}
+          featured
+          onPress={() => navigate("Dishdetail", { dishId: item.id })}
+          imageSrc={{ uri: baseUrl + item.image }}
         />
       );
     };
-
-    return (
-      <FlatList 
-      data={this.props.dishes.dishes}
-      renderItem={renderMenuItem}
-      keyExtractor={item => item.id.toString()}
-      />
-    );
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      );
+    }
   }
 }
 
