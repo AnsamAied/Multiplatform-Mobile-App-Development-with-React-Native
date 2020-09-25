ac1a1ef78bfb3943354a2083907b5a42ea11ad05
mohammad-abuhummos <mohammad.abuhummoscs@gmail.com>
2020-09-25 19:30:42 +0300

    Using Redux in your React Native App

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -22,7 +22,24 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import Dishdetail from "./DishdetailComponent";
 import { Icon } from "react-native-elements";
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchDishes: () => dispatch(fetchDishes()),
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders()),
+})
 const MainNavigatorStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -218,6 +235,13 @@ function MainNavigator() {
   );
 }
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
+    this.props.fetchLeaders();
+  }
+  
   render() {
     return (
       <NavigationContainer>
@@ -251,4 +275,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
